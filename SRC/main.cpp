857ea4615bd4e1013cd0e304cc817b37abf2a842
VanessaELungu <vanessa.e.lungu@accenture.com>
2021-11-07 20:02:49 +0200

    main

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <memory>
+
+#include "Proxy.h"
+#include "Doc.h"
+
+using namespace std;
+
+int main()
+{
+    Document doc;
+
+    doc.open();
+    doc.changePage(4);
+    doc.changePage(17);
+    doc.changePage(24);
+    doc.changePage(1);
+    doc.changePage(37);
+    
+
+    return 0;
+}
